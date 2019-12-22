4b443add2be5673682680c87bda5bcf2d903593c
riandwisusanto <riandwisusanto03@gmail.com>
2019-12-22 14:44:42 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,448 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Pejam Studio</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="logo.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Regna
+    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <a href="#hero"><img src="logo.png" alt="" title="Pejam" style="width:32px" /></img></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="#hero">Regna</a></h1>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Beranda</a></li>
+          <li><a href="#about">Tentang Kami</a></li>
+          <li><a href="#services">Layanan</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Tim</a></li>
+          <!-- <li class="menu-has-children"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="menu-has-children"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li> -->
+          <li><a href="#contact">Kontak</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Hero Section
+  ============================-->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>SELAMAT DATANG DI PEJAM</h1>
+      <h2>Kami menawarkan solusi bisnis melalui teknologi digital dengan layanan yang maksimal dan harga yang kompetitif</h2>
+      <a href="#about" class="btn-get-started">Mulai</a>
+    </div>
+  </section><!-- #hero -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="title">Tentang Kami</h2>
+            <p>
+              konten informasi
+            </p>
+            <div>
+        		<h5>Yang kami tawarkan</h5>
+        		<li><a href="#services"><i class="fa fa-check mr-2"></i></a>Website Development</li>
+        		<li><a href="#services"><i class="fa fa-check mr-2"></i></a>Website Designing</li>
+        		<li><a href="#services"><i class="fa fa-check mr-2"></i></a>Mobile Development</li>
+        		<li><a href="#services"><i class="fa fa-check mr-2"></i></a>Company Profile</li>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    <!--==========================
+      Facts Section
+    ============================-->
+    <section id="facts">
+      <div class="container wow fadeIn">
+        <div class="row counters">
+
+  				<div class="col-sm-4 col-6 text-center">
+            <span data-toggle="counter-up">40</span>
+            <p>Clients</p>
+  				</div>
+
+          <div class="col-sm-4 col-6 text-center">
+            <span data-toggle="counter-up">53</span>
+            <p>Projects</p>
+  				</div>
+
+          <div class="col-sm-4 col-12 text-center">
+            <span data-toggle="counter-up">4</span>
+            <p>Team</p>
+  				</div>
+
+  			</div>
+
+      </div>
+    </section><!-- #facts -->
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          <h3 class="section-title">Layanan</h3>
+          <br>
+          <br>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="box">
+              <div class="icon"><a href="#contact"><i class="fa fa-desktop"></i></a></div>
+              <h4 class="title"><a href="#contact">Website Development</a></h4>
+              <p class="description">konten tentang mobile dev</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+            <div class="box">
+              <div class="icon"><a href="#contact"><i class="fa fa-mobile"></i></a></div>
+              <h4 class="title"><a href="#contact">Mobile Development</a></h4>
+              <p class="description">konten tentang mobile dev</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+            <div class="box">
+              <div class="icon"><a href="#contact"><i class="fa fa-cogs"></i></a></div>
+              <h4 class="title"><a href="#contact">Optimization</a></h4>
+              <p class="description">Optimalkan pemasaran produk anda dengan teknologi dan pengalaman kami hingga jangkauan yang tidak terbayangkan oleh anda</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #services -->
+
+    <!--==========================
+    Call To Action Section
+    ============================-->
+    <section id="call-to-action">
+      <div class="container wow fadeIn">
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-left">
+            
+            <p class="cta-title">Mencari solusi yang tepat untuk bisnis anda ?</p>
+            <p class="cta-text">Kembangkan potensi bisnis anda dengan Pejam</p>>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#contact">Hubungi Kami</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #call-to-action -->
+
+    <!--==========================
+      Portfolio Section
+    ============================-->
+    <section id="portfolio">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h3 class="section-title">Portfolio</h3>
+        </div>
+        <br>
+        <div class="row">
+
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter=".filter-app, .filter-card, .filter-logo, .filter-web" class="filter-active">All</li>
+              <li data-filter=".filter-app">App</li>
+              <li data-filter=".filter-web">Web</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" id="portfolio-wrapper">
+          <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+            <a href="">
+              <img src="img/portfolio/app1.jpg" alt="">
+              <div class="details">
+                <h4>App 1</h4>
+                <span>Alored dono par</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-web">
+            <a href="">
+              <img src="img/portfolio/web2.jpg" alt="">
+              <div class="details">
+                <h4>Web 2</h4>
+                <span>Alored dono par</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+            <a href="">
+              <img src="img/portfolio/app3.jpg" alt="">
+              <div class="details">
+                <h4>App 3</h4>
+                <span>Alored dono par</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-web">
+            <a href="">
+              <img src="img/portfolio/web1.jpg" alt="">
+              <div class="details">
+                <h4>Card 1</h4>
+                <span>Alored dono par</span>
+              </div>
+            </a>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </section><!-- #portfolio -->
+
+    <!--==========================
+      Team Section
+    ============================-->
+    <section id="team">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h3 class="section-title">Tim Kami</h3>
+          <br><br>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="img/tim1.jpg" alt=""></div>
+              <h4>Rian Dwi</h4>
+              <span>Back-end Engineer</span>
+              <div class="social">
+                <a href="https://web.facebook.com/rdwisusanto1"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/riandwisusanto/"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="img/tim2.jpg" alt=""></div>
+              <h4>Iqbalul Hidayat</h4>
+              <span>Front-end Engineer</span>
+              <div class="social">
+                <a href="https://web.facebook.com/iqbalul.hidayat"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/iqbalulhidayat/"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="img/tim3.jpg" alt=""></div>
+              <h4>Sulistyanto Laili</h4>
+              <span>Designer/Sistem Analis</span>
+              <div class="social">
+                <a href="https://web.facebook.com/laili.beastmaniak"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/slaillyr_/"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="img/tim4.jpg" alt=""></div>
+              <h4>Salsabila Maharani</h4>
+              <span>Designer/Sistem Analis</span>
+              <div class="social">
+                <a href="https://web.facebook.com/salsabila.herlambang"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/salsabilaherlambang/"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #team -->
+
+    <!--==========================
+      Contact Section
+    ============================-->
+    <section id="contact">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h3 class="section-title">Kontak</h3>
+          <br><br>
+        </div>
+      </div>
+
+      <!-- Uncomment below if you wan to use dynamic maps -->
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.3325028958941!2d112.7252702979201!3d-7.3167760243588145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb7abf69cd55%3A0x1eb3cb3df2eb4572!2sGedung%20A1%20Fakutas%20Teknik%20Informatika%20Unesa!5e0!3m2!1sid!2sid!4v1576901798923!5m2!1sid!2sid" width="100%" height="360" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+      <div class="container wow fadeInUp mt-5">
+        <div class="row justify-content-center">
+
+          <div class="col-lg-3 col-md-4">
+
+            <div class="info">
+              <div>
+                <i class="fa fa-map-marker"></i>
+                <p>Ketintang, Surabaya</p>
+              </div>
+
+              <div>
+              	<a href=""><i class="fa fa-envelope"></i></a>
+                <p>belumtau@gmail.com</p>
+              </div>
+
+              <div>
+                <a href=""><i class="fa fa-phone"></i></a>
+                <p>+62 858 5385 855</p>
+              </div>
+            </div>
+
+            <div class="social-links">
+              <a href="https://facebook.com" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="https://instagram.com" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="https://wa.me/08585385855?text=Silahkan%20bicarakan%20bisnis%20anda%20dengan%20kami" class="whatsapp"><i class="fa fa-whatsapp"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <div class="form">
+              <div id="sendmessage">Pesan telah terkirim. Terima kasih!</div>
+              <div id="errormessage"></div>
+              <form action="" method="post" role="form" class="contactForm">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="nama" placeholder="Nama anda" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email anda" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subjek" id="subject" placeholder="subjek" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="Pesan" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Pesan"></textarea>
+                  <div class="validation"></div>
+                </div>
+                <div class="text-center"><button type="submit">Kirim Pesan</button></div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Pejam</strong>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
